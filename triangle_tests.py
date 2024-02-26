8250f34859003b2fd98b2f165bb7028b955f7c31
Ethan Wilson <ehwilson.business@gmail.com>
2024-02-25 20:56:54 -0500

    completed all test_is_<triangle type> tests. completed test_str. completed test_eq_1, test_eq_2

--- triangle_tests.py
+++ triangle_tests.py
@@ -15,15 +15,18 @@ class TriangleTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             Triangle("3","4","5")
 
+
     # test for ValueError exception when creating and initializing a Triangle object: side(s) not positive
     def test_init_3(self):
         with self.assertRaises(ValueError):
             Triangle(-3,-4,-5)
 
+
     # test for ValueError exception when creating and initializing a Triangle object: invalid triagle
     def test_init_4(self):
         with self.assertRaises(ValueError):
             Triangle(20,2,2)
+            
             
     # test for "Equilateral" type of triangle
     def test_type_1(self):
@@ -31,11 +34,13 @@ class TriangleTest(unittest.TestCase):
         t_string = t.type()
         self.assertEqual(t_string, 'Equilateral')
         
+        
     # test for "Isosceles" type of triangle
     def test_type_2(self):
         t = Triangle(5,5,6)
         t_string = t.type()
         self.assertEqual(t_string, 'Isosceles')
+
 
     # test for "Scalene" type of triangle
     def test_type_3(self):
@@ -43,45 +48,60 @@ class TriangleTest(unittest.TestCase):
         t_string = t.type()
         self.assertEqual(t_string, 'Scalene')
     
+    
     # test for checking if a triangle is equilateral
     def test_is_equilateral_1(self):
         t = Triangle(3,3,3)
         self.assertTrue(t.is_equilateral())
 
+
     # test for checking if a triangle is not equilateral
     def test_is_equilateral_2(self):
-        pass
+        t = Triangle(5,5,6)
+        self.assertFalse(t.is_equilateral())
+
 
     # test for checking if a triangle is isosceles
     def test_is_isoceles_1(self):
         t = Triangle(5,5,6)
         self.assertTrue(t.is_isosceles())
 
+
     # test for checking if a triangle is not isosceles
     def test_is_isoceles_2(self):
-        pass
-
+        t = Triangle(3,4,5)
+        self.assertFalse(t.is_isosceles())
+        
+        
     # test for checking if a triangle is scalene
     def test_is_scalene_1(self):
         t = Triangle(3,4,5)
         self.assertTrue(t.is_scalene())
 
+
     # test for checking if a triangle is not scalene
     def test_is_scalene_2(self):
-        pass
+        t = Triangle(3,3,3)
+        self.assertFalse(t.is_scalene())
+
 
     # test if calling str() on a Triangle object returns expected string representation
     # of Triangle object (as list of sides)
     def test_str(self):
-        pass
+        t = Triangle(3,4,5)
+        self.assertEqual(t.__str__(), f"[{t.s1},{t.s2},{t.s3}]")
 
     # test if two Triangle objects with same three sides returns True
     def test_eq_1(self):
-        pass
+        t1 = Triangle(3,4,5)
+        t2 = Triangle(3,4,5)
+        self.assertTrue(t1.__eq__(t2))
 
     # test if two Triangle objects with different sides returns False
     def test_eq_2(self):
-        pass
+        t1 = Triangle(3,4,5)
+        t2 = Triangle(4,5,6)
+        self.assertFalse(t1.__eq__(t2))
 
     # test if a Triangle object is equal to itself (i.e., returns True)
     def test_eq_3(self):
